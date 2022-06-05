d7479723c1eea3a89204a209faa7defb51a66bc2
vaultec <47548474+vaultec81@users.noreply.github.com>
2022-06-05 13:06:08 -0700

    fix: misc types

--- src/types/posts.ts
+++ src/types/posts.ts
@@ -16,6 +16,9 @@ export interface PostHive {
         tags: string[]
         app: string
     }
+
+    updated_at: Date
+    created_at: Date
 }
 
 export interface PostSpk {
@@ -37,4 +40,51 @@ export interface PostSpk {
 
 export interface VideoSpk extends PostSpk {
 
+}
+
+
+
+interface DesktopVideo {
+
+    title: string
+    description: string
+    tags: string[]
+
+    sourceMap: Array<{type: string, url: string, format: string}>
+    filesize: number
+    created: string
+    video: {
+        duration: number
+        info: {
+            author: string
+            permlink: string
+        }
+    }
+    app: string
+    type: string
+}
+
+interface ThreespeakVideo {
+    app: string
+    type: string
+    image: string[]
+    video: {
+        info: {
+            platform: '3speak'
+            title: string
+            author: string
+            permlink: string
+            duration: number
+            filesize: number
+            file: string
+            lang: string
+            firstUpload: boolean
+            ipfs: string | null
+            ipfsThumbnail: string | null
+        }
+        content: {
+            description: string
+            tags: string[]
+        }
+    }
 }
